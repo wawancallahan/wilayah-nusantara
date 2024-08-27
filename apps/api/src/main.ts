7200455e9e8a7bf9743d93923d6e05f4a22b1127
wawancallahan <wawan.callahan@gmail.com>
2024-08-27 10:37:45 +0700

    adjust cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from '~/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders:"*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: "*"
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(1945);
